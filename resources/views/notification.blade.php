20ae27de94b7f7b33a440f09e513032f1768e21c
Frederick Sandalo <uscbsitric@gmail.com>
2017-03-15 21:52:09 +0800

    Finished Building the Notification UI

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>Real-Time Laravel with Pusher</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     
     <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,200italic,300italic" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="http://d3dhju7igb20wy.cloudfront.net/assets/0-4-0/all-the-things.css" />
@@ -79,6 +79,47 @@
 	}
 
 	$(notifyInit);
+
+	// Use toastr to show the notification
+	function showNotification(data)
+	{
+      // TODO: get the text from the event data <--- I wonder why this tutorial does not have the code for it
+      var text = data.text;
+
+      // TODO: use the text in the notification <--- I wonder why this tutorial does not have the code for it
+      toastr.options = {"closeButton": false,
+    	                "debug": false,
+    	                "newestOnTop": false,
+    	                "progressBar": true,
+    	                "positionClass": "toast-top-right",
+    	                "preventDuplicates": false,
+    	                "onclick": null,
+    	                "showDuration": "300",
+    	                "hideDuration": "1000",
+    	                "timeOut": "5000",
+    	                "extendedTimeOut": "1000",
+    	                "showEasing": "swing",
+    	                "hideEasing": "linear",
+    	                "showMethod": "fadeIn",
+    	                "hideMethod": "fadeOut"
+        	          }
+      toastr.success(text);
+      //toastr.info('Are you the 6 fingered man?');  // if you want it in INFO type, known types are Success, Info, Warning, Error
+    }
+
+	var pusher = new Pusher('{{env("PUSHER_KEY")}}',
+			                {
+		                      cluster: '{{env("PUSHER_CLUSTER")}}',
+		                      encrypted: true
+			                }
+			               );
+
+	// TODO: Subscribe to the channel <--- I wonder why this tutorial does not have the code for it
+	var channel = pusher.subscribe('notifications'); // subscribe to the "notifications" channel
+
+	// TODO: Bind to the event and pass in the notification handler <--- I wonder why this tutorial does not have the code for it
+    channel.bind('new-notification', showNotification);
+
 	</script>
   </body>
 </html>
